c2a34597ffb2f64b1cd90f2e821eca7852b7d873
thavin8220 <thavinmurugan@gmail.com>
2022-12-14 11:43:37 +0530

    add file

---
+++ main.js
@@ -0,0 +1,12 @@
+let c=0;
+var a=parseInt(prompt("Enter the length"));
+for(i=1; i<=a;i++){
+    var b=parseInt(prompt("enter the value"));
+if(b>0){
+     c=c+b;
+   
+}
+}
+ document.write(c);
+
+
